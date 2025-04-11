3ea9619124685476f2ff1911b96391d74a1da0b9
andrea <andrea.gitcommit@gmail.com>
2025-04-10 22:50:25 -0400

    Comment out colleague changes

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -37,7 +37,7 @@ cp ./data/raw/*user_log_*.log ./data/processed/user_logs
 cp ./data/raw/*event_log_*.log ./data/processed/event_logs
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
 rm ./data/raw/*ipaddr* ./data/raw/rawdata/*ipaddr* ./data/processed/*ipaddr*
-# rf -rf ./data
+# FATAL - rm -rf ./data
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 touch ./data/inventory.txt
